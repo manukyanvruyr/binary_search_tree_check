305ed6a6ed8b785a5e989e17bb531de720eb1a61
vruyr <32260169+manukyanvruyr@users.noreply.github.com>
2018-02-19 23:41:19 +0400

    BFS

---
+++ test.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+#include "binary_tree.hpp"
+#include "utility.hpp"
+
+namespace
+{
+
+bool binary_search_tree()
+{
+	binary_tree<int> t1{15, 5, 20, 2, 6, 18, 23};
+	binary_tree<int> t2{15, 5, 20, 2, 17, 18, 23};
+	binary_tree<int> t3{1, 5};
+	binary_tree<int> t4{5};
+	return is_binary_search_tree(t1) && !is_binary_search_tree(t2)
+		&& !is_binary_search_tree(t3) && is_binary_search_tree(t4);
+}
+
+bool BFS_test()
+{
+	binary_tree<int> t1{15, 5, 20, 2, 6, 18, 23};
+	auto v1 = BFS(t1);
+	std::vector<int> r1{15, 5, 20, 2, 6, 18, 23};
+	bool t = v1 == r1;
+	binary_tree<int> t2{15, 5, 20, 2};
+	auto v2 = BFS(t2);
+	std::vector<int> r2{15, 5, 20, 2};
+	t = t && v2 == r2;
+	return t;
+}
+
+}
+
+namespace test
+{
+
+void run()
+{
+	bool t = binary_search_tree();
+	t = t && BFS_test();
+	if (t) {
+		std::cout << "Passed\n";
+	} else {
+		std::cout << "Failed\n";
+	}
+}
+
+}
--- utility.hpp
+++ utility.hpp
@@ -1,20 +1,47 @@
 #pragma once
 
 #include <limits>
+#include <queue>
+
 #include "binary_tree.hpp"
 
-template<typename T>
-bool is_binary_search_tree(Node<T>* const n, const T& max, const T& min)
+template <typename T>
+bool is_binary_search_tree(Node<T>* const n, Node<T>* const left, Node<T>* const right)
 {
-	if (n->value > max || n->value < min || (nullptr != n->left && !is_binary_search_tree(n->left, n->value, min))
-			|| (nullptr != n->right && !is_binary_search_tree(n->right, max, n->value))) {
+	if ((nullptr != left && n->value < left->value) || (nullptr != right && n->value > right->value)
+		|| (nullptr != n->left && !is_binary_search_tree(n->left, left, n))
+		|| (nullptr != n->right && !is_binary_search_tree(n->right, n, right))) {
 		return false;
 	}
 	return true;
 }
 
-template<typename T>
+template <typename T>
 bool is_binary_search_tree(const binary_tree<T>& t)
 {
-	return is_binary_search_tree(t.get_root(), std::numeric_limits<T>::max(), std::numeric_limits<T>::min());
+	return is_binary_search_tree(t.get_root(), static_cast<Node<T>*>(nullptr), static_cast<Node<T>*>(nullptr));
+}
+
+template <typename T>
+std::vector<T> BFS(const binary_tree<T>& t)
+{
+	std::vector<int> v;
+	auto root = t.get_root();
+	if (nullptr == root) {
+		return v;
+	}
+    std::queue<Node<T>*> q;
+    q.push(root);
+    while (!q.empty()) {
+        auto node = q.front();
+        v.push_back(node->value);
+        q.pop();
+        if (nullptr != node->left) {
+            q.push(node->left);
+        }
+        if (nullptr != node->right) {
+            q.push(node->right);
+        }
+    }
+    return v;
 }
